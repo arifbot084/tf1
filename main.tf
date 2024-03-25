cf79de4259f065689964bad29ec1c5eb1087b510
arifbot084 <162156591+arifbot084@users.noreply.github.com>
2024-03-26 05:09:17 +0530

    conflict

--- main.tf
+++ main.tf
@@ -19,31 +19,4 @@ resource "aws_subnet" "mysubnet" {
   }
   
 }
-resource "aws_subnet" "mysubnet2" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.10.20.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "mysubnet"
-  }
-  
-}
 
-
-
-resource "aws_dynamodb_table" "example" {
-  name           = "example-table"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "Id"
-
-  attribute {
-    name = "Id"
-    type = "N"
-  }
-
-  tags = {
-    Environment = "production"
-    Name        = "example-table"
-  }
-}
