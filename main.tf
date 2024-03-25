209251aae6941e0e4f2e28f0baac10c795e4c7cf
arifbot084 <162156591+arifbot084@users.noreply.github.com>
2024-03-26 01:31:12 +0530

    test2

--- main.tf
+++ main.tf
@@ -2,15 +2,7 @@
   region = "us-east-1"
   
 }
-/*
-resource "aws_instance" "test_instance" {
- ami           = "ami-0c101f26f147fa7fd"
- instance_type = "t2.micro"
- tags = {
-   Name = "test1_instance"
- }
-}
-*/
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.10.0.0/16"
   tags = {
